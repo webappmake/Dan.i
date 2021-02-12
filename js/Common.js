410b9068117f94860a6950a7a57b1f1b4e2082cb
naridasom <naridasom90@gmail.com>
2021-02-13 05:55:31 +0900

    vs git test

--- js/Common.js
+++ js/Common.js
@@ -1,145 +1,91 @@
-﻿$(document).ready(function () {
-    // modal custom
-    $('.modal').on('show.bs.modal', function (e) {
-        //alert("팝업 오픈");
-    })
-
-    $('.modal').on('hidden.bs.modal', function (e) {
-        //alert("팝업 닫힘");
-        chatbot_box();
-    });
-
-    // 스크롤 여부에 따라 top 버튼 생성
-    $(window).scroll(function () {
-        setTimeout(scroll_top, 0);//화살표가 반응하여 생기는 시간
-    });
-
-    $(".btn_top").click(function () {
-        $("html, body").animate({ scrollTop: 0 }, 400);//화살표 클릭시 화면 스크롤 속도
-        return false;
-    });
-
-    // 스크롤이 멈추면 5초 후 top 버튼 숨기기
-    $.fn.scrollStopped = function (callback) {
-        var that = this, $this = $(that);
-        $this.scroll(function (ev) {
-            clearTimeout($this.data('scrollTimeout'));
-            $this.data('scrollTimeout', setTimeout(callback.bind(that), 3000, ev));
-        });
-    };
-
-    $(window).scrollStopped(function () {
-        $(".btn_top").fadeOut("slow");
-    });
-
-
-    // gnb 활성화
-    $(".gnb_menu > li").on("hover mouseenter mouseover", function () {
-        $(".gnb_menu > li").removeClass("active");
-        if ($(this).hasClass("active")) {
-            // 클래스 있음
-            $(this).removeClass("active")
-        } else {
-            // 클래스 없음
-            $(".gnb_menu > li").removeClass("active");
-            $(this).addClass("active");
-        }
-    });
-
-    $(".gnb_menu > li").on("mouseout mouseleave", function () {
-        $(".gnb_menu > li").removeClass("active");
-        if ($(this).hasClass("active")) {
-            // 클래스 있음
-            $(this).removeClass("active")
-        } else {
-            // 클래스 없음
-            $(".gnb_menu > li").removeClass("active");
-            $(this).addClass("active")
-        }
-    });
-
-    // Tab
-    $('.con_tab > li').click(function () {
-        var activeTab = $(this).attr('rel');
-        $('.con_tab > li').removeClass('active');
-        $('.con_tab_box').removeClass('active');
-        $(this).addClass('active');
-        $('#' + activeTab).addClass('active');
-        load_chatbox();
-    });
-
-    $('.con_sub_tab > li').click(function () {
-        var activeTab = $(this).attr('rel');
-        $('.con_sub_tab > li').removeClass('active');
-        $('.con_sub_tab_box').removeClass('active');
-        $(this).addClass('active');
-        $('#' + activeTab).addClass('active');
-        load_chatbox();
-    });
-
-    $('.modal_tab > li').click(function () {
-        var activeTab = $(this).attr('rel');
-        $('.modal_tab > li').removeClass('active');
-        $('.modal_tab_box').removeClass('active');
-        $(this).addClass('active');
-        $('#' + activeTab).addClass('active');
-    });
-
-    $('.modal_sub_tab > li').click(function () {
-        var activeTab = $(this).attr('rel');
-        $('.modal_sub_tab > li').removeClass('active');
-        $('.modal_sub_tab_box').removeClass('active');
-        $(this).addClass('active');
-        $('#' + activeTab).addClass('active');
-    });
-
-    // Topic Toggle
-    $('.rel_toggle').click(function () {
-        if ($(this).find("span").hasClass('active')) {
-            $(this).next().hide();
-            $(this).find("span").text("토픽보기").removeClass('active');
-        }
-        else {
-            $(".rel_topic_list").hide();
-            $('.rel_toggle').find("span").text("토픽보기").removeClass('active');
-            $(this).next().show();
-            $(this).find("span").text("토픽접기").addClass('active');
-        }
-    });
-
-    // Tbl Topic Toggle
-    $('.icon_tbl_toggle').click(function () {
-        if ($(this).hasClass('active')) {
-            $(this).parents("tr").next().hide();
-            $(this).text("토픽보기").removeClass('active');
-        }
-        else {
-            $(".tbl_topic_area").hide();
-            $('.icon_tbl_toggle').text("토픽보기").removeClass('active');
-            $(this).parents("tr").next().show();
-            $(this).text("토픽접기").addClass('active');
-        }
-    });
-
-    if (location.hash == "#RecommendSubject") {
-        var activeTab = $("#RecommendSubject").attr('rel');
-
-        $(window).scrollTop(0);
-        $('.con_tab').find("li").removeClass("active");
-        $('.con_tab_box').removeClass('active');
-        $('.con_tab').find('#RecommendSubject').addClass('active')
-        $('#' + activeTab).addClass('active');
-
+﻿// Toggle
+function fn_Toggle(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).removeClass('active');
     }
+    else {
+        $(obj).addClass('active');
+    }
+}
 
-    // modal backdrop multiple issue
-    $(document).on('show.bs.modal', '.modal', function (event) {
-        var zIndex = 1040 + (10 * $('.modal:visible').length);
-        $(this).css('z-index', zIndex);
-        setTimeout(function () {
-            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
-        }, 0);
-    });
-});
+// more class 관련 Toggle
+function fn_MoreToggle(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).next().hide();
+        $(obj).removeClass('active');
+    }
+    else {
+        $(obj).next().show();
+        $(obj).addClass('active');
+    }
+}
 
+// tab 관련 Toggle
+function fn_TabToggle(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).siblings().removeClass("active");
+    }
+    else {
+        $(obj).siblings().removeClass("active");
+        $(obj).addClass('active');
+    }
+}
 
+// ON & OFF Toggle
+function fn_OnOff(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).text('OFF').removeClass('active');
+        $(obj).prev().removeClass('active');
+    }
+    else {
+        $(obj).text('ON').addClass('active');
+        $(obj).prev().addClass('active');
+    }
+}
+
+// 알림 팝업 창 닫기
+function fn_NotiClose() {
+    $(".ico_noti").removeClass("active");
+    $(".noti_pop_area").hide();
+}
+
+// 챗봇 입력 창 열기
+function fn_Chatbot_Open(obj) {
+    $(obj).parent().addClass("active");
+}
+
+// 챗봇 입력 창 닫기
+function fn_Chatbot_Close() {
+    $(".btn_chatbot").removeClass("active");
+}
+
+// 챗봇 팝업 창 열기
+function fn_chat_pop_open(id) {
+    $("#pop_" + id).show();
+}
+
+// 챗봇 팝업 창 닫기
+function fn_chat_pop_close(id) {
+    $("#pop_" + id).hide();
+}
+
+// filter area close
+function fn_filterClose() {
+    $(".ico_filter").removeClass("active");
+    $(".filter_pop_area").hide();
+}
+
+// 셀렉트 새 창으로 열기
+function go_url(url) {
+    if (url != '') window.open(url, '_blank');
+}
+
+// top 버튼 클릭시 상단으로 이동
+function scroll_top() {
+    if ($(window).scrollTop() <= 1) {
+        $(".btn_top").fadeOut("slow");
+    }
+    else {
+        $(".btn_top").fadeIn("fast");
+    }
+};
---
+++ js/index.js
@@ -0,0 +1,145 @@
+﻿$(document).ready(function () {
+    // modal custom
+    $('.modal').on('show.bs.modal', function (e) {
+        //alert("팝업 오픈");
+    })
+
+    $('.modal').on('hidden.bs.modal', function (e) {
+        //alert("팝업 닫힘");
+        chatbot_box();
+    });
+
+    // 스크롤 여부에 따라 top 버튼 생성
+    $(window).scroll(function () {
+        setTimeout(scroll_top, 0);//화살표가 반응하여 생기는 시간
+    });
+
+    $(".btn_top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 400);//화살표 클릭시 화면 스크롤 속도
+        return false;
+    });
+
+    // 스크롤이 멈추면 5초 후 top 버튼 숨기기
+    $.fn.scrollStopped = function (callback) {
+        var that = this, $this = $(that);
+        $this.scroll(function (ev) {
+            clearTimeout($this.data('scrollTimeout'));
+            $this.data('scrollTimeout', setTimeout(callback.bind(that), 3000, ev));
+        });
+    };
+
+    $(window).scrollStopped(function () {
+        $(".btn_top").fadeOut("slow");
+    });
+
+
+    // gnb 활성화
+    $(".gnb_menu > li").on("hover mouseenter mouseover", function () {
+        $(".gnb_menu > li").removeClass("active");
+        if ($(this).hasClass("active")) {
+            // 클래스 있음
+            $(this).removeClass("active")
+        } else {
+            // 클래스 없음
+            $(".gnb_menu > li").removeClass("active");
+            $(this).addClass("active");
+        }
+    });
+
+    $(".gnb_menu > li").on("mouseout mouseleave", function () {
+        $(".gnb_menu > li").removeClass("active");
+        if ($(this).hasClass("active")) {
+            // 클래스 있음
+            $(this).removeClass("active")
+        } else {
+            // 클래스 없음
+            $(".gnb_menu > li").removeClass("active");
+            $(this).addClass("active")
+        }
+    });
+
+    // Tab
+    $('.con_tab > li').click(function () {
+        var activeTab = $(this).attr('rel');
+        $('.con_tab > li').removeClass('active');
+        $('.con_tab_box').removeClass('active');
+        $(this).addClass('active');
+        $('#' + activeTab).addClass('active');
+        load_chatbox();
+    });
+
+    $('.con_sub_tab > li').click(function () {
+        var activeTab = $(this).attr('rel');
+        $('.con_sub_tab > li').removeClass('active');
+        $('.con_sub_tab_box').removeClass('active');
+        $(this).addClass('active');
+        $('#' + activeTab).addClass('active');
+        load_chatbox();
+    });
+
+    $('.modal_tab > li').click(function () {
+        var activeTab = $(this).attr('rel');
+        $('.modal_tab > li').removeClass('active');
+        $('.modal_tab_box').removeClass('active');
+        $(this).addClass('active');
+        $('#' + activeTab).addClass('active');
+    });
+
+    $('.modal_sub_tab > li').click(function () {
+        var activeTab = $(this).attr('rel');
+        $('.modal_sub_tab > li').removeClass('active');
+        $('.modal_sub_tab_box').removeClass('active');
+        $(this).addClass('active');
+        $('#' + activeTab).addClass('active');
+    });
+
+    // Topic Toggle
+    $('.rel_toggle').click(function () {
+        if ($(this).find("span").hasClass('active')) {
+            $(this).next().hide();
+            $(this).find("span").text("토픽보기").removeClass('active');
+        }
+        else {
+            $(".rel_topic_list").hide();
+            $('.rel_toggle').find("span").text("토픽보기").removeClass('active');
+            $(this).next().show();
+            $(this).find("span").text("토픽접기").addClass('active');
+        }
+    });
+
+    // Tbl Topic Toggle
+    $('.icon_tbl_toggle').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).parents("tr").next().hide();
+            $(this).text("토픽보기").removeClass('active');
+        }
+        else {
+            $(".tbl_topic_area").hide();
+            $('.icon_tbl_toggle').text("토픽보기").removeClass('active');
+            $(this).parents("tr").next().show();
+            $(this).text("토픽접기").addClass('active');
+        }
+    });
+
+    if (location.hash == "#RecommendSubject") {
+        var activeTab = $("#RecommendSubject").attr('rel');
+
+        $(window).scrollTop(0);
+        $('.con_tab').find("li").removeClass("active");
+        $('.con_tab_box').removeClass('active');
+        $('.con_tab').find('#RecommendSubject').addClass('active')
+        $('#' + activeTab).addClass('active');
+
+    }
+
+    // modal backdrop multiple issue
+    $(document).on('show.bs.modal', '.modal', function (event) {
+        var zIndex = 1040 + (10 * $('.modal:visible').length);
+        $(this).css('z-index', zIndex);
+        setTimeout(function () {
+            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+        }, 0);
+    });
+});
+
+
