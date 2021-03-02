91be07ffffd91687bb3f6d6407543a6aaf9d7db8
유현정/(협력사) 과장/DXT센터 서비스플랫폼담당 클라우드플랫폼TP(hyunjung.ryu@lgepartner.com) <hyunjung.ryu@lge.com>
2021-03-02 23:24:03 +0900

    캠퍼스라이프 완료 > vw 변환전

--- js/index.js
+++ js/index.js
@@ -173,14 +173,23 @@
     })
 
      // 캠퍼스라이프 gnb 활성화
-     $(".camp_menu > li").on("hover mouseenter mouseover", function () {
-        $(".camp_menu > li").removeClass("active");
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active")
-        } else {
-            $(".camp_menu > li").removeClass("active");
-            $(this).addClass("active");
-        }
+     $('.camp_menu > li').click(function(){
+        var num=$(this).index();
+
+        $('.camp_menu > li').removeClass('active');
+        $(this).addClass('active');
+
+        $('.modal_body > section').removeClass('active');
+        $('.modal_body > section').eq(num).addClass('active');
+        return false;
+
+
+        // if ($(this).hasClass("active")) {
+        //     $(this).removeClass("active")
+        // } else {
+        //     $(".camp_menu > li").removeClass("active");
+        //     $(this).addClass("active");
+        // }
     });
 });
 
