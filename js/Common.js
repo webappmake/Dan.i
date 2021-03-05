9528b3586088e8776e3d613a9deef9ba388ae8b1
유현정/(협력사) 과장/DXT센터 서비스플랫폼담당 클라우드플랫폼TP(hyunjung.ryu@lgepartner.com) <hyunjung.ryu@lge.com>
2021-03-05 16:07:07 +0900

    add all

---
+++ js/Common.js
@@ -0,0 +1,118 @@
+﻿// Toggle
+function fn_Toggle(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).removeClass('active');
+    }
+    else {
+        $(obj).addClass('active');
+    }
+}
+
+// more class 관련 Toggle
+function fn_MoreToggle(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).next().hide();
+        $(obj).removeClass('active');
+    }
+    else {
+        $(obj).next().show();
+        $(obj).addClass('active');
+    }
+}
+
+// tab 관련 Toggle
+function fn_TabToggle(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).siblings().removeClass("active");
+    }
+    else {
+        $(obj).siblings().removeClass("active");
+        $(obj).addClass('active');
+    }
+}
+
+// ON & OFF Toggle
+function fn_OnOff(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).text('OFF').removeClass('active');
+        $(obj).prev().removeClass('active');
+    }
+    else {
+        $(obj).text('ON').addClass('active');
+        $(obj).prev().addClass('active');
+    }
+}
+
+// 알림 팝업 창 닫기
+function fn_NotiClose() {
+    $(".ico_noti").removeClass("active");
+    $(".noti_pop_area").hide();
+}
+
+// ListSequence Toggle
+function fn_ListSequence(obj) {
+    if ($(obj).hasClass('active')) {
+        $(obj).removeClass('active');
+    }
+    else {
+        $(obj).addClass('active');
+    }
+};
+
+// 채용정보 분석 미니 팝업 오픈
+function fn_mini_pop_open(obj) {
+    $(obj).next(".mini_popup_wrap").addClass("active");
+    var minipop_wid = ($(obj).next().find(".mini_popup").outerWidth() / 2) - 10;
+    var minipop_hei = $(obj).next().find(".mini_popup").outerHeight() + 40;
+    
+    
+    $(obj).next().find(".mini_popup").css({ "right": -minipop_wid, "top": -minipop_hei });
+}
+
+// 채용정보 분석 미니 팝업 닫기
+function fn_mini_pop_close(obj) {
+    $(obj).parents(".mini_popup_wrap").removeClass("active");
+}
+
+
+// filter area close
+function fn_filterClose() {
+    $(".ico_filter").removeClass("active");
+    $(".filter_pop_area").hide();
+}
+
+// 셀렉트 새 창으로 열기
+function go_url(url) {
+    if (url != '') window.open(url, '_blank');
+}
+
+// top 버튼 클릭시 상단으로 이동
+function scroll_top() {
+    if ($(".contents").scrollTop() <= 1) {
+        $(".btn_top").fadeOut("slow");
+    }
+    else {
+        $(".btn_top").fadeIn("fast");
+    }
+};
+
+// 토스트 팝업 실행
+function fn_Toast() {
+    $(".toast").toast('show')
+}
+
+// 가입프로세스 피드백 등록 영역
+function fn_feedback_show(obj) {
+    $(obj).next().show();
+}
+
+// 페이지마다 메뉴 개수가 다르고 즐겨찾기, 설정과 같이 메뉴가 우측에 있어 잘려보이는 경우에 실행되도록 해주시면 됩니다.
+// 디바이스 가로 크기보다 메뉴 가로 크기가 더 큰 경우 크기 구해서 페이지 로드시 위치 이동
+function nav_position() {
+    var vwValue = 1024 * 3.90625 / 100; // vw 변환
+
+    var nav_wid = $(".sub_menu").outerWidth() + vwValue;
+    var swiper_position = - (nav_wid - $(".nav_select_swiper").outerWidth()) + "px";
+
+    $(".sub_menu").css("transform", "translate3d(" + swiper_position + ", 0, 0px)");
+}
---
+++ js/index.js
@@ -0,0 +1,135 @@
+﻿$(document).ready(function () {
+    // modal custom
+    $('.modal').on('shown.bs.modal', function (e) {
+        //alert("팝업 오픈");        
+    })
+
+    $('.modal').on('hidden.bs.modal', function (e) {
+        //alert("팝업 닫힘");
+    });
+
+    // tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // tooltip sm custom
+    $('.tooltip_sm').on('shown.bs.tooltip', function () {
+        $(".tooltip").addClass("tooltip-sm");
+    })
+
+    $('.tooltip_sm').on('hidden.bs.tooltip', function () {
+        $(".tooltip").removeClass("tooltip-sm");
+    })
+
+    // 스크롤 여부에 따라 top 버튼 생성
+    $(".contents").scroll(function () {
+        setTimeout(scroll_top, 0);//화살표가 반응하여 생기는 시간
+    });
+
+    $(".btn_top").click(function () {
+        $(".contents").animate({ scrollTop: 0 }, 400);//화살표 클릭시 화면 스크롤 속도
+        return false;
+    });
+
+    // 스크롤이 멈추면 5초 후 top 버튼 숨기기
+    $.fn.scrollStopped = function (callback) {
+        var that = this, $this = $(that);
+        $this.scroll(function (ev) {
+            clearTimeout($this.data('scrollTimeout'));
+            $this.data('scrollTimeout', setTimeout(callback.bind(that), 3000, ev));
+        });
+    };
+
+    $(".contents").scrollStopped(function () {
+        $(".btn_top").fadeOut("slow");
+    });
+
+    // Topic Toggle
+    $('.topic_btn').click(function () {
+        if ($(this).find("strong").hasClass('active')) {
+            $(this).parent().next().hide();
+            $(this).find("strong").text("토픽보기").removeClass('active');
+        }
+        else {
+            // $(".topic_list").hide();
+            // $(".topic_list_wrap").hide();
+            $(this).find("strong").text("토픽보기").removeClass('active');
+            $(this).parent().next().show();
+            $(this).find("strong").text("토픽접기").addClass('active');
+        }
+    });
+
+    // Topic Toggle
+    $('.topic_recruit_btn').click(function () {
+        if ($(this).find("strong").hasClass('active')) {
+            $(this).parent().next().hide();
+            $(this).find("strong").removeClass('active');
+        }
+        else {
+            $(this).find("strong").removeClass('active');
+            $(this).parent().next().show();
+            $(this).find("strong").addClass('active');
+        }
+    });
+
+    // Tbl Topic Toggle
+    $('.icon_tbl_toggle').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).parents("tr").next().hide();
+            $(this).text("토픽보기").removeClass('active');
+        }
+        else {
+            $(".tbl_topic_area").hide();
+            $('.icon_tbl_toggle').text("토픽보기").removeClass('active');
+            $(this).parents("tr").next().show();
+            $(this).text("토픽접기").addClass('active');
+        }
+    });
+
+    // 필터 영역 이외의 영역 클릭시 필터 영역 닫기
+    $(document).mouseup(function (e) {
+        var ico_filter = $(".btn_select");
+        var filterPopup = $(".filter_wrap");
+        if (filterPopup.has(e.target).length === 0) {
+            ico_filter.removeClass("active").next().hide();
+        }
+    });
+
+    // modal backdrop multiple issue
+    $(document).on('show.bs.modal', '.modal', function (event) {
+        var zIndex = 1040 + (10 * $('.modal:visible').length);
+        $(this).css('z-index', zIndex);
+        setTimeout(function () {
+            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+        }, 0);
+    });
+
+    //전공이수에 따른 졸업자수 .status_area
+    $('.status_list > li').click(function(){
+        var num=$(this).index();
+
+        $('.status_list > li').removeClass('active');
+        $(this).addClass('active');
+        $('.status_area > .status_cont > li').removeClass('active');
+        $('.status_area > .status_cont > li').eq(num).addClass('active');
+        return false;
+    })
+
+     // 캠퍼스라이프 gnb 활성화
+     $('.camp_menu > li').click(function(){
+        var num=$(this).index();
+
+        $('.camp_menu > li').removeClass('active');
+        $(this).addClass('active');
+        $('.modal_body > section').removeClass('active');
+         $('.modal_body > section').eq(num).addClass('active');
+         if ($(this).hasClass("camp_menu_lg")) {
+             $(".camp_wrap").removeClass("camp_wrap01");
+         } else {
+             $(".camp_wrap").addClass("camp_wrap01");
+         }
+        return false;
+    });
+    
+});
+
+
